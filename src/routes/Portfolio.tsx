4f6dc6002e13dc3cc12f4c735b9547f3a836e344
Alfred Roy M <59073967+slimer37@users.noreply.github.com>
2026-05-09 01:24:12 -0700

    show controls in iframes

--- src/routes/Portfolio.tsx
+++ src/routes/Portfolio.tsx
@@ -7,7 +7,7 @@ import './portfolio.css'
 
 function EmbedVideo(videoId: string){
   return <iframe width="100%" height="100%"
-    src={"https://www.youtube.com/embed/" + videoId + "?controls=0"}
+    src={"https://www.youtube.com/embed/" + videoId}
     title="YouTube video player"
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
     referrerPolicy="strict-origin-when-cross-origin"
